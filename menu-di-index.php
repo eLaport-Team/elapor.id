fa6b315669e0b8521445bf45fee75153cf458caa
pangirkirankreatif <113571841+pangirkirankreatif@users.noreply.github.com>
2022-09-17 10:55:23 +0700

    Create menu-di-index.php
    Menu Diindex

---
+++ menu-di-index.php
@@ -0,0 +1,7 @@
+<ul class="nav navbar-nav">
+						<li><a id="GoToHome" href="#" onclick="getPage('home')" class="selected">Home</a></li>
+						<li><a id="GoToFitur" href="#tentangsimakdesa">Tentang SIMAKD<span style="text-transform: lowercase !important;">esa</span></a></li>	
+						<li><a id="GoToFitur" href="#fitur">Fitur</a></li>						
+						<li><a id="GoToGallery" href="#penggunasimakdesa">Pengguna SIMAKD<span style="text-transform: lowercase !important;">esa</span></a></li>
+						<li><a id="GoToContact" href="#" onclick="getPage('hubungi')">Hubungi Kami</a></li>													
+					</ul>
